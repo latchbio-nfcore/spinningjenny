5f4f4a4523eac697e6ec755edcaec8cd6f25c414
Tahir D'Mello <tahirdme@gmail.com>
2024-06-17 20:00:56 -0700

    Automated commit: processed repository spinningjenny

---
+++ latch_metadata/parameters.py
@@ -0,0 +1,66 @@
+
+from dataclasses import dataclass
+import typing
+import typing_extensions
+
+from flytekit.core.annotation import FlyteAnnotation
+
+from latch.types.metadata import NextflowParameter
+from latch.types.file import LatchFile
+from latch.types.directory import LatchDir, LatchOutputDir
+
+# Import these into your `__init__.py` file:
+#
+# from .parameters import generated_parameters
+
+generated_parameters = {
+    'input': NextflowParameter(
+        type=LatchFile,
+        default=None,
+        section_title='Input/output options',
+        description='Path to comma-separated file containing information about the variables used in the experiment.',
+    ),
+    'template': NextflowParameter(
+        type=typing.Optional[LatchFile],
+        default=None,
+        section_title=None,
+        description='Path to xml file containing information about experiment to be run using the ABM',
+    ),
+    'nlogo': NextflowParameter(
+        type=typing.Optional[LatchFile],
+        default=None,
+        section_title=None,
+        description='Path to the nlogo file containing information about the ABM',
+    ),
+    'batches': NextflowParameter(
+        type=typing.Optional[int],
+        default=1,
+        section_title=None,
+        description='Number of batches for running chunks of experiments',
+    ),
+    'outdir': NextflowParameter(
+        type=typing_extensions.Annotated[LatchDir, FlyteAnnotation({'output': True})],
+        default=None,
+        section_title=None,
+        description='The output directory where the results will be saved. You have to use absolute paths to storage on Cloud infrastructure.',
+    ),
+    'email': NextflowParameter(
+        type=typing.Optional[str],
+        default=None,
+        section_title=None,
+        description='Email address for completion summary.',
+    ),
+    'multiqc_title': NextflowParameter(
+        type=typing.Optional[str],
+        default=None,
+        section_title=None,
+        description='MultiQC report title. Printed as page header, used for filename if not otherwise specified.',
+    ),
+    'multiqc_methods_description': NextflowParameter(
+        type=typing.Optional[str],
+        default=None,
+        section_title='Generic options',
+        description='Custom MultiQC yaml file containing HTML including a methods description.',
+    ),
+}
+
